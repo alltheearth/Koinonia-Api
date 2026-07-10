110f1fe36488870b073413bd15368acd939ae620
alltheearth <alltheearth.tech@gmail.com>
2026-07-09 22:07:04 -0300

    Remove legacy Node/Express files (replaced by Django project)

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`API rodando na porta ${PORT}`);
-});
