6757e3cac13830d43dad2908fe7472aa20e773df
alltheearth <alltheearth.tech@gmail.com>
2026-07-09 22:07:12 -0300

    Remove legacy Node/Express files (replaced by Django project)

--- src/routes/contatos.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getContatos, postContato } from "../controllers/contatosController";
-
-const contatosRoutes = Router();
-
-contatosRoutes.get("/", getContatos);
-contatosRoutes.post("/", postContato);
-
-export default contatosRoutes;
